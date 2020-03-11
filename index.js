a74119a0bc22172ecb1ee263a7fa48e4f85bc36c
cli333 <cml8198@protonmail.com>
2020-03-10 23:01:24 -0700

    update

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const cors = require("cors");
 const app = express();
 const { URI } = require("./config/keys");
 const api = require("./routes/api");
-
-const port = process.env.PORT || 5000;
+const path = require("path");
 
 app.use(cors());
 app.use(express.json());
@@ -18,5 +17,14 @@ mongoose
 
 // use routes
 app.use("/api", api);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port:${port}`));
